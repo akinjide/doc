89b5aad03cd9ceadfa66765e12aab456867a9349
andela-abankole <akinjide.bankole@andela.co>
2015-11-10 16:05:13 +0100

    test (Suite): contain test suite and spec for fend test

---
+++ tests/unit/controllers/MainController.spec.js
@@ -0,0 +1,17 @@
+describe('example test', function() {
+  it('should be true', function() {
+    expect('foo').toBe('foo');
+  });
+  it('should be true', function() {
+    expect('foo').toBe('foo');
+  });
+});
+
+describe('example test', function() {
+  it('should be true', function() {
+    expect('foo').toBe('bar');
+  });
+  it('should be true', function() {
+    expect('foo').toBe('foo');
+  });
+});
