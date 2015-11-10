da0a9e26960e87791e8dae189ea5748255f85f48
andela-abankole <akinjide.bankole@andela.co>
2015-11-10 17:00:09 +0100

    test (Suite): update test suite and spec for bend test

--- tests/server/user.spec.js
+++ tests/server/user.spec.js
@@ -29,7 +29,6 @@ describe('Document Management System : User Route', function() {
   frisby.create('POST /api/users `should create a new user instance`')
     .post('http://localhost:3000/api/users', user)
     .expectStatus(200)
-    .inspectJSON()
     .expectJSONTypes('response', {
        username : String,
        name : {
@@ -54,7 +53,6 @@ describe('Document Management System : User Route', function() {
       password : 'U)bo5uwgSp'
     })
     .expectStatus(200)
-    .inspectJSON()
     .expectJSON({
       "status" : "OK",
       "statusMessage" : "login successful",
@@ -71,25 +69,47 @@ describe('Document Management System : User Route', function() {
     })
     .toss()
 // END
- 
+
 // POST /api/users/logout `should log a user out`
-  // frisby.create('POST /api/users/logout `should log a user in`')
-  //   .post('http://localhost:3000/api/users/logout', {
-  //     username : 'naddacvek',
-  //     password : 'U)bo5uwgSp'
-  //   })
-  //   .addHeader('x-access-token', token)
-  //   .expectStatus(200)
-  //   .inspectJSON()
-  //   .expectJSON({
-  //     "status" : "OK",
-  //     "statusCode" : 200,
-  //     "statusMessage" : "logout successful",
-  //   })
-  //   .toss()
+  frisby.create('POST')
+    .post('http://localhost:3000/api/users/login', {
+      username : 'narutosama',
+      password : '90732FHFG3'
+    })
+    .afterJSON(function(json){
+      // Second test, run after first is completed
+      frisby.create('POST /api/users/logout `should log a user in`')
+        .post('http://localhost:3000/api/users/logout', {
+          username : 'naddacvek',
+          password : 'U)bo5uwgSp'
+        })
+        .addHeader('x-access-token', token)
+        .expectStatus(200)
+        .inspectJSON()
+        .expectJSON({
+          "status" : "OK",
+          "statusCode" : 200,
+          "statusMessage" : "logout successful",
+        })
+        .toss()
+    })
+    .toss()
 // END
 
 // GET /api/users `should find all users`
+  var user2 = {
+    username : 'narutosama',
+    name : {
+      first : 'Narutokun',
+      last  : 'Uzumaki'
+    },
+    email    : 'narutokun@ragok.com',
+    password : '90732FHFG3'
+  };
+
+  UserID = user2.id;
+  token = jwt.sign(user2, config.secret);
+
   frisby.create('POST')
     .post('http://localhost:3000/api/users', {
       username : chance.word({syllables: 3}),
@@ -102,16 +122,28 @@ describe('Document Management System : User Route', function() {
     })
     .expectStatus(200)
     .toss()
-  frisby.create('GET /api/users `should find all users`')
-     .get('http://localhost:3000/api/users')
-     .addHeader('x-access-token', token)
-     .inspectJSON()
-     .expectStatus(200)
-     .expectJSON({
-       "statusCode" : 200,
-       "status" : 'OK'
-     })
-  .toss()
+  frisby.create('POST')
+    .post('http://localhost:3000/api/users', user2)
+    .toss()
+  frisby.create('POST')
+    .post('http://localhost:3000/api/users/login', {
+      username : 'narutosama',
+      password : '90732FHFG3'
+    })
+    .afterJSON(function(json){
+      // Second test, run after first is completed
+      frisby.create('GET /api/users `should find all users`')
+       .get('http://localhost:3000/api/users')
+       .addHeader('x-access-token', token)
+       .expectStatus(200)
+       .inspectJSON()
+       .expectJSON({
+         "statusCode" : 200,
+         "status" : 'OK'
+       })
+      .toss()
+    })
+    .toss()
 // END 
  
 // GET /users/<id> `should find a user`
@@ -127,116 +159,136 @@ describe('Document Management System : User Route', function() {
     })
     .expectStatus(200)
     .toss()
-  frisby.create('GET /api/users `should find a users`')
-     .get('http://localhost:3000/api/users')
-     .addHeader('x-access-token', token)
-     .inspectJSON()
-     .expectStatus(200)
-     .expectJSON({
-       "statusCode" : 200,
-       "status" : 'OK'
-     })
-     .afterJSON(function(json){
-       // Second test, run after first is completed
-       frisby.create('GET')
-          .get('http://localhost:3000/api/users/' + json.response[0]._id)
-          .addHeader('x-access-token', token)
-          .inspectJSON()
-          .expectStatus(200)
-          .expectJSON({
-            "status" : "OK",
-            "statusCode" : 200,
-            response: {
-              username : 'kayak',
-              name : {
-                first : 'Kaneki',
-                last  : 'Kun'
-              },
-              email   : 'kaneki@kun.com'
-            }
-          })
-          .expectJSONTypes({
-            response: {
-              username : String,
-              name : {
-                first : String,
-                last  : String
-              },
-              email   : String
-            }
-          })
-          .toss()
-     })
-  .toss()
+  frisby.create('POST')
+    .post('http://localhost:3000/api/users/login', {
+      username : 'narutosama',
+      password : '90732FHFG3'
+    })
+    .afterJSON(function(json){
+      frisby.create('GET /api/users `should find a users`')
+         .get('http://localhost:3000/api/users')
+         .addHeader('x-access-token', token)
+         .expectStatus(200)
+         .expectJSON({
+           "statusCode" : 200,
+           "status" : 'OK'
+         })
+         .afterJSON(function(json){
+           // Second test, run after first is completed
+           frisby.create('GET')
+              .get('http://localhost:3000/api/users/' + json.response[0]._id)
+              .addHeader('x-access-token', token)
+              .expectStatus(200)
+              .expectJSON({
+                "status" : "OK",
+                "statusCode" : 200,
+                response: {
+                  username : 'kayak',
+                  name : {
+                    first : 'Kaneki',
+                    last  : 'Kun'
+                  },
+                  email   : 'kaneki@kun.com'
+                }
+              })
+              .expectJSONTypes({
+                response: {
+                  username : String,
+                  name : {
+                    first : String,
+                    last  : String
+                  },
+                  email   : String
+                }
+              })
+            .toss()
+         })
+      .toss()
+    })
+    .toss()
 // END
 
 // PUT /users/<id> `should update user attributes`
-  frisby.create('GET')
-     .get('http://localhost:3000/api/users')
-     .addHeader('x-access-token', token)
-     .inspectJSON()
-     .expectStatus(200)
-     .expectJSON({
-       "statusCode" : 200,
-       "status" : 'OK'
-     })
-     .afterJSON(function(json){
-       // Second test, run after first is completed
-       frisby.create('PUT /users/<id> `should update user attributes`')
-          .put('http://localhost:3000/api/users/' + json.response[1]._id, {
-            username : chance.word({syllables: 3}),
-            name : {
-              first : chance.first(),
-              last  : chance.last()
-            },
-            email   : chance.email()
-          })
-          .addHeader('x-access-token', token)
-          .inspectJSON()
-          .expectStatus(200)
-          .expectJSON({
-            "status" : "OK",
-            "statusCode" : 200,
-            "statusMessage" : "user update successful",
-            response: {
-              "ok": 1,
-              "nModified": 1,
-              "n": 1
-            }
-          })
-          .toss()
-     })
-  .toss()
+  frisby.create('POST')
+    .post('http://localhost:3000/api/users/login', {
+      username : 'narutosama',
+      password : '90732FHFG3'
+    })
+    .afterJSON(function(json){
+      frisby.create('GET')
+         .get('http://localhost:3000/api/users')
+         .addHeader('x-access-token', token)
+         .expectStatus(200)
+         .expectJSON({
+           "statusCode" : 200,
+           "status" : 'OK'
+         })
+         .afterJSON(function(json){
+           // Second test, run after first is completed
+           frisby.create('PUT /users/<id> `should update user attributes`')
+              .put('http://localhost:3000/api/users/' + json.response[1]._id, {
+                username : chance.word({syllables: 3}),
+                name : {
+                  first : chance.first(),
+                  last  : chance.last()
+                },
+                email   : chance.email()
+              })
+              .addHeader('x-access-token', token)
+              .expectStatus(200)
+              .expectJSON({
+                "status" : "OK",
+                "statusCode" : 200,
+                "statusMessage" : "user update successful",
+                response: {
+                  "ok": 1,
+                  "nModified": 1,
+                  "n": 1
+                }
+              })
+              .toss()
+         })
+      .toss()
+    })
+    .toss()
 // END
 
 // DELETE /users/<id> `should delete user`
-  frisby.create('GET')
-     .get('http://localhost:3000/api/users')
-     .addHeader('x-access-token', token)
-     .inspectJSON()
-     .expectStatus(200)
-     .expectJSON({
-       "statusCode" : 200,
-       "status" : 'OK'
-     })
-     .afterJSON(function(json){
-       // Second test, run after first is completed
-       frisby.create('DELETE /users/<id> `should delete user`')
-          .delete('http://localhost:3000/api/users/' + json.response[1]._id)
-          .addHeader('x-access-token', token)
-          .inspectJSON()
-          .expectStatus(200)
-          .expectJSON({
-            "status" : "OK",
-            "statusCode" : 200,
-            "statusMessage" : "user delete successful",
-            response: {
-             "ok": 1,
-             "n": 1
-            }
-          })
-          .toss()
-     })
+  frisby.create('POST')
+    .post('http://localhost:3000/api/users/login', {
+      username : 'narutosama',
+      password : '90732FHFG3'
+    })
+    .afterJSON(function(json) {
+      frisby.create('GET')
+         .get('http://localhost:3000/api/users')
+         .addHeader('x-access-token', token)
+         .inspectJSON()
+         .expectStatus(200)
+         .expectJSON({
+           "statusCode" : 200,
+           "status" : 'OK'
+         })
+         .afterJSON(function(json){
+           // Second test, run after first is completed
+           frisby.create('DELETE /users/<id> `should delete user`')
+              .delete('http://localhost:3000/api/users/' + json.response[1]._id)
+              .addHeader('x-access-token', token)
+              .inspectJSON()
+              .expectStatus(200)
+              .expectJSON({
+                "status" : "OK",
+                "statusCode" : 200,
+                "statusMessage" : "user delete successful",
+                response: {
+                 "ok": 1,
+                 "n": 1
+                }
+              })
+              .toss()
+         })
+      .toss()
+    })
   .toss()
 // END
 });
