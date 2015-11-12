6472f892e206f796ea137c7eb11916e07bf54dbd
andela-abankole <akinjide.bankole@andela.co>
2015-11-12 12:15:53 +0100

    style (config): whitespaces and semicolon

--- server/config/config.js
+++ server/config/config.js
@@ -2,4 +2,4 @@ module.exports = {
   'database' : process.env.MONGO_DEV || process.env.MONGO_PROD,
   'secret'   : process.env.SECRET_DEV || process.env.SECRET_PROD,
   'key'      : process.env.KEY_DEV || process.env.KEY_PROD
-}
+};
