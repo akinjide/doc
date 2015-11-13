d097b6cec01258ddb953ee09d8a5687b2d127386
andela-abankole <akinjide.bankole@andela.co>
2015-11-13 10:34:05 +0100

    style (service): add comments to user and document service

--- app/scripts/services/user.service.js
+++ app/scripts/services/user.service.js
@@ -7,6 +7,12 @@ angular.module('myApp')
   .value('userAPI', '/api/users')
   .factory('UserService', ['$http', '$log', 'userAPI', function($http, $log, userAPI) {
     return {
+      
+      /**
+       * [createUser POST creates a user]
+       * @param     {[Object]}     params [contains user details to create]
+       * @return    {[Object]}            [a promise from API]
+       */
       createUser : function(params) {
         return $http.post(userAPI, { 
                                     username   : params.username, 
@@ -19,22 +25,46 @@ angular.module('myApp')
                                   });
       },
 
+      /**
+       * [userLogin POST logs in a user]
+       * @param     {[Object]}       param [contains user details to login]
+       * @return    {[Object]}             [a promise from API]
+       */
       userLogin : function(param) {
         return $http.post(userAPI + '/login', { username : param.username, password : param.password });
       },
 
+      /**
+       * [userLogout POST logs out a user]
+       * @return    {[Object]}         [a promise from API]
+       */
       userLogout : function() {
         return $http.post(userAPI + '/logout');
       },
 
+      /**
+       * [getUsers GET all users]
+       * @return    {[Object]}      [a promise from API]
+       */
       getUsers : function() {
         return $http.get(userAPI);
       },
 
+      /**
+       * [getUser GET a user details]
+       * @param     {[String]}    userID [contains the logged in userID]
+       * @return    {[Object]}           [a promise from API]
+       */
       getUser : function(userID) {
         return $http.get(userAPI + '/' + userID);
       },
 
+      /**
+       * [updateUser PUT a user]
+       * @param     {[String]}    userID [contains the logged in userID]
+       * @param     {[Object]}    params [contains user details to update]
+       * @return    {[Object]}           [a promise from API]
+       */
       updateUser : function(userID, params) {
         return $http.put(userAPI + '/' + userID, { 
                                             username   : params.username, 
@@ -46,10 +76,20 @@ angular.module('myApp')
                                           });
       },
 
+      /**
+       * [deleteUser DELETE a user]
+       * @param     {[String]}      userID [contains the logged in userID]
+       * @return    {[Object]}             [a promise from API]
+       */
       deleteUser : function(userID) {
         return $http.delete(userAPI + '/' + userID);
       },
 
+      /**
+       * [getUserDocuments GET a user documents]
+       * @param     {[String]}     userID [contains the logged in userID]
+       * @return    {[Object]}            [a promise from API]
+       */
       getUserDocuments : function(userID) {
         return $http.get(userAPI + '/' + userID + '/documents');
       },
